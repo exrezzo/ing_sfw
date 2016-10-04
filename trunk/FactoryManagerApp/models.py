9259337475130203a3333d852eb079506f408829
g.papa10 <g.papa10@dc828973-fadb-4491-bffb-f75388e33df1>
2016-10-04 10:31:15 +0000

    creazione modello Strumento

--- trunk/FactoryManagerApp/models.py
+++ trunk/FactoryManagerApp/models.py
@@ -1,4 +1,20 @@
 from django.db import models
+## creazione modello strumento con id_ambiente come attributo chiave esterna##
+
+
+class Strumento(models.Model):
+
+        #id_ambiente = models.ForeignKey(Ambiente)
+        nome_strumento = models.CharField(max_length=56)
+        modello = models.CharField(max_length=56)
+        marca = models.CharField(max_length=56)
+        anno_acquisto = models.CharField(max_length=56)
+        tipologia = models.CharField(max_length=56)
+
+        def __str__(self):
+            return self.modello + self.nome_strumento + self.marca
+
+
 
 # Aggiungo dizionario per sesso
 SESSO = (
@@ -25,3 +41,4 @@ class Dipendente(models.Model):
     telefono = models.CharField(max_length=16)
     domicilio = models.CharField(max_length=32)
     idMansione = models.ForeignKey(Mansione)
+
