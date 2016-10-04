7c560ce521d1d341ebaf11cef20d6c3694f2e588
m.tamma8 <m.tamma8@dc828973-fadb-4491-bffb-f75388e33df1>
2016-10-04 10:21:40 +0000

    #3837 #3843 completate

--- trunk/FactoryManagerApp/models.py
+++ trunk/FactoryManagerApp/models.py
@@ -1,4 +1,27 @@
 from django.db import models
 
+# Aggiungo dizionario per sesso
+SESSO = (
+    ('M' , 'Maschio'),
+    ('F' , 'Femmina'),
+)
+
 # Create your models here.
-#modifica by giorgio
+
+# 'Mansione'
+# @param nome: denominazione mansione eseguita dal Dipendente
+class Mansione(models.Model):
+    nome=models.CharField(max_length=32)
+
+# 'Dipendente'
+# definizione attributi generici di dipendente
+class Dipendente(models.Model):
+    nome = models.CharField(max_length=32)
+    cognome = models.CharField(max_length=32)
+    sesso = models.CharField(max_length=7, choices=SESSO)
+    dataNascita = models.DateField()
+    codiceFiscale = models.CharField(max_length=16)
+    email = models.CharField(max_length=32)
+    telefono = models.CharField(max_length=16)
+    domicilio = models.CharField(max_length=32)
+    idMansione = models.ForeignKey(Mansione)
