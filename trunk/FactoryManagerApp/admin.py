50a2af74b9ae9177072cce9ba9e12459ba33b8ad
m.tamma8 <m.tamma8@dc828973-fadb-4491-bffb-f75388e33df1>
2016-10-05 11:03:29 +0000

    #3851 closed

--- trunk/FactoryManagerApp/admin.py
+++ trunk/FactoryManagerApp/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from .models import *
 
+class DipendenteUtilizzaInLine (admin.TabularInline):
+    model = Utilizza
+    extra = 3
+
+class DipendenteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cognome', 'idMansione', 'email')
+    inlines = [DipendenteUtilizzaInLine]
+
+
+
+
 # Register your models here.
 
 admin.site.register(Strumento)
-admin.site.register(Dipendente)
+admin.site.register(Dipendente, DipendenteAdmin)
 admin.site.register(Mansione)
 admin.site.register(Lavora)
 admin.site.register(Utilizza)
