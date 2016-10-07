aea530c9f3d1ef442600161d3d5eab9914fe8c15
exrezzo <exrezzo@users.noreply.github.com>
2016-10-07 13:06:53 +0200

    Update settings.py

--- FactoryManagerProject/settings.py
+++ FactoryManagerProject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'site-packages.suit',
+    'suit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
